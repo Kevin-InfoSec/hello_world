f9dfef64f870c537519b68822150f55c35fb9687
spectre1037 <36394152+spectre1037@users.noreply.github.com>
2020-08-07 17:50:27 -0400

    Add files via upload

--- LoopBasics.py
+++ LoopBasics.py
@@ -6,31 +6,59 @@ def LoopEx(): #Demontrate for and while loops iterating through themselves
     LoopType = input().lower()
     while LoopType != 'while' and LoopType != 'for':
         LoopType = input('Please enter your loop type (while OR for) ').lower()
+    test = 'failure'
     if LoopType == 'for':
         print('How many values in your FOR Loop? You can have 1 to 3 arguments passed to a for loop. Each works slightly differently')
-        Val = int(input())
-        while Val <1 or Val >3:
-              print('Please enter a valid i value')
-              Val = int(input())
+        Val = input()
+        while Val !='1' and Val !='2' and Val !='3':
+            Val = input('Please enter a valid i value (1,2, or 3) ')
+        Val = int(Val)
         if Val == 1:
             print('Your syntax is for i in range(x)')
-            print('This will return x times')
-            x = int(TakeInput('x'))
+            print('This will return x times (starting at 0)')
+            x = -1
+            while test == 'failure':
+                try:
+                    x = int(TakeInput('valid positive integer for x'))
+                    if x >= 1:
+                        test = 'success'
+                except:
+                    continue
             for i in range(x):
                 print('i = ' + str(i))
         elif Val == 2:
             print('Your syntax is for i in range(x,y)')
-            print('This will return starting at x through y')
-            x = int(TakeInput('x'))
-            y = int(TakeInput('y'))
+            print('This will return starting at x through y (x must be less than y)')
+            while test == 'failure':
+                try:
+                    x = int(TakeInput('valid integer for x'))
+                    y = int(TakeInput('valid integer for y'))
+                    if x >= y:
+                        print('x must be less than y')
+                    else:
+                        test = 'success'
+                except:
+                    continue
             for i in range(x,y):
                 print('i = ' + str(i))
         elif Val == 3:
             print('Your syntax is for i in range(x,y,z)')
             print('This will return starting at x through y counting by z')
-            x = int(TakeInput('x'))
-            y = int(TakeInput('y'))
-            z = int(TakeInput('z'))
+            while test == 'failure':
+                try:
+                    x = int(TakeInput('valid integer for x'))
+                    y = int(TakeInput('valid integer for y'))
+                    z = int(TakeInput('valid integer for z'))
+                    if z == 0:
+                        print('A zero iterator will cause an infinite loop!')
+                    elif x >= y and z > 0:
+                        print('x must be less than y with a positive iterator')
+                    elif x <= y and z < 0:
+                        print('x must be greater than y with a negative iterator')
+                    else:
+                        test = 'success'
+                except:
+                    continue
             for i in range(x,y,z):
                 print('i = ' + str(i))
         else:
@@ -39,14 +67,30 @@ def LoopEx(): #Demontrate for and while loops iterating through themselves
         print('Your syntax is while x <comparator> y')
         print('We will also define an iterator')
         print('This will return as long as x <comparator> y is true your iterator will determine how x is modified each step. Please note we will only be evaluating numeric while loops, there are many other ways to use while loops')
-        x = int(TakeInput('x'))
+        while test == 'failure':
+            try:
+                x = int(TakeInput('valid integer x'))
+                test = 'success'
+            except:
+                continue
         comparator = TakeInput('comparator')
-        y = int(TakeInput('y'))
-        iterator = int(TakeInput('iterator'))
+        test2 = 'failure'
+        while test2 == 'failure':
+            try:
+                y = int(TakeInput('valid integer y'))
+                test2 = 'success'
+            except:
+                continue
+        test3 = 'failure'
+        while test3 == 'failure':
+            try:
+                iterator = int(TakeInput('valid positive integer iterator'))
+                if iterator > 0:
+                    test3 = 'success'
+            except:
+                continue
         while comparator != '==' and comparator != '!=' and comparator != '>' and comparator != '<' and comparator != '>':
             comparator = TakeInput('valid comparator (==,!=,>,<, (we are skipping >= and <=)')
-        while iterator <= 0:
-            iterator = TakeInput('valid iterator (a number greater than 0)')
         if comparator == '==':
             if x == y:
                 while x == y:
@@ -77,5 +121,7 @@ def LoopEx(): #Demontrate for and while loops iterating through themselves
                 x+= iterator
         else:
             print('You broke my program! ' + comparator + ' is not a valid comparator!')
+    else:
+        print('How did you break my input validation?')
 LoopEx() # You can call this as many times as you like for testing purposes
 
